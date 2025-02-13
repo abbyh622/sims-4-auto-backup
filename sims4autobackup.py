25020916ef47d9f1c75ac2a192938878a33a0562
abbyh622 <abbyh622@gmail.com>
2025-02-12 18:42:20 -0600

    fixed updating files

--- sims4autobackup.py
+++ sims4autobackup.py
@@ -14,6 +14,7 @@ from googleapiclient.errors import HttpError
 #                   add progress bar for each folder
 
 SCOPES = ["https://www.googleapis.com/auth/drive"]
+ACCT_DATA_FILE = "accountDataDB.package"
 creds = None
 gameDir = os.path.expanduser("~\\Documents\\Electronic Arts\\The Sims 4")   # default game directory, should be universal i think
 
@@ -33,11 +34,14 @@ def main():
     creds = authenticateGoogleDrive()
     logging.basicConfig(level=logging.INFO, format="%(message)s")
     
-    # make option to change configs if loaded
     # get gameDir path and backup settings from config file
     if os.path.exists("config.json"):
         with open("config.json", "r") as file:
-            configs = json.load(file)
+            # prompt user to confirm/change
+            configs = promptConfigs(json.load(file))
+        with open("config.json", "w") as file:
+            json.dump(configs, file) 
+    # if no config file, apply defaults and prompt user
     else:
         defaultConfigs = {"gameDir": gameDir, "backupSaves": True, "backupTray": True, "backupAccountData": True, "backupMods": False}
         configs = promptConfigs(defaultConfigs)
@@ -47,34 +51,48 @@ def main():
     try:
         service = build("drive", "v3", credentials=creds)
         # if backup folder exists, get id and datetime last modified
-        # not working, always creates new folder
-        response = service.files().list(q="name='Sims 4 Backup' and mimeType='application.vnd/google-apps.folder'", spaces='drive').execute()
+        # need to specify trashed=false otherwise it will include trashed folders
+        response = service.files().list(q="name='Sims 4 Backup' and mimeType='application/vnd.google-apps.folder' and trashed=false", spaces='drive').execute()
         if response['files']:   # response is a dictionary
             folderId = response['files'][0].get("id")
+            logging.info("Backup folder found")
         # else create backup folder
         else: 
             metadata = {"name": "Sims 4 Backup", "mimeType": "application/vnd.google-apps.folder"}
             file = service.files().create(body=metadata, fields="id").execute()
             folderId = file.get("id")
+
+            logging.info("Backup folder not found; created new folder")
         
         if configs['backupSaves']:
-            logging.info(blue + "Uploading save files . . ." + reset)
+            logging.info(blue + clearLine + "Uploading save files . . ." + reset)
             backupFolder(service, "saves", folderId)
         if configs['backupTray']:
-            logging.info(blue + "Uploading tray files . . ." + reset)
+            logging.info(blue + clearLine + "Uploading tray files . . ." + reset)
             backupFolder(service, "Tray", folderId)
         if configs['backupMods']:
-            logging.info(blue + "Uploading mods folder . . ." + reset)
+            logging.info(blue + clearLine + "Uploading mods folder . . ." + reset)
             backupFolder(service, "Mods", folderId)           
-
+        if configs['backupAccountData']:
         # backup accountdatadb
-        logging.info(blue + "Uploading account data file . . ." + reset)
-        localdb = os.path.join(gameDir, "accountDataDB.package")
-        if os.path.exists(localdb) and os.path.isfile(localdb):
-            metadata = {"name": os.path.basename(localdb), "parents": [folderId]}
-            media = MediaFileUpload(localdb, resumable=True)
-            file = service.files().create(body=metadata, media_body=media, fields="id").execute()
-            print(yellow + f"Uploaded: {os.path.basename(localdb)}" + reset, end = "\r")
+        # not checking modified time bc its a small enough file and seems to update every time game is closed
+            logging.info(blue + clearLine + "Uploading account data file . . ." + reset)
+            localdb = os.path.join(gameDir, ACCT_DATA_FILE)
+            if os.path.exists(localdb) and os.path.isfile(localdb):
+                response = service.files().list(q=f"'{folderId}' in parents and name='{ACCT_DATA_FILE}' and trashed=false", spaces='drive').execute()
+                # update file
+                if response['files']:
+                    fileId = response['files'][0].get("id")
+                    media = MediaFileUpload(localdb, resumable=True)
+                    service.files().update(fileId=fileId, media_body=media).execute()
+                    print(yellow + f"Uploaded: {os.path.basename(localdb)}" + reset, end = "\r")
+                # upload as new file
+                else:
+                    metadata = {"name": os.path.basename(localdb), "parents": [folderId]}
+                    media = MediaFileUpload(localdb, resumable=True)
+                    file = service.files().create(body=metadata, media_body=media, fields="id").execute()
+                    print(yellow + f"Uploaded: {os.path.basename(localdb)}" + reset, end = "\r")
+
         print(magenta + clearLine + "Backup complete :)" + reset)
             
     except HttpError as e:
@@ -104,15 +122,17 @@ def backupFolder(service, curFolder, mainFolder):
         return
     
     # check if folder exists, get id and datetime last modified
-    response = service.files().list(q=f"'{mainFolder}' in parents and name='{curFolder}' and mimeType='application/vnd.google-apps.folder'", spaces='drive').execute()
+    response = service.files().list(q=f"'{mainFolder}' in parents and name='{curFolder}' and mimeType='application/vnd.google-apps.folder' and trashed=false", spaces='drive').execute()
     if response['files']:
         curId = response['files'][0].get("id")
     else:
         metadata = {"name": f"{curFolder}", "mimeType": "application/vnd.google-apps.folder", "parents": [mainFolder]}
         file = service.files().create(body=metadata, fields="id").execute()
         curId = file.get("id")
+ 
     # get last modified datetime of saves folder
     response = service.files().list(q=f"'{curId}' in parents", spaces='drive', fields="files(modifiedTime)", orderBy="modifiedTime desc").execute()
+
     # convert to timwzone aware datetime object for comparison
     lastModified = response['files'][0].get("modifiedTime") if response['files'] else None
     if lastModified:
@@ -120,20 +140,32 @@ def backupFolder(service, curFolder, mainFolder):
     else:
         # set to epoch time if no files in folder
         driveLastModified = datetime.fromtimestamp(0, tz=timezone.utc)
-    logging.info(f"Last backed up: {driveLastModified.split("+")}") 
+
+    print(f"Last backed up: {str(driveLastModified)}", end = "\r") 
 
     # use os.scandir() to iterate through each folder 
     localFolder = os.path.join(gameDir, curFolder)
     for entry in os.scandir(localFolder):
-        # check if entry is a file
+        # get last modified time
         if entry.is_file():
             localLastModified = datetime.fromtimestamp(os.path.getmtime(entry.path), tz=timezone.utc)
-            # if local file modified > drive file, upload replacing existing
-            if localLastModified > driveLastModified:
+            # check if file exists in drive
+            response = service.files().list(q=f"'{curId}' in parents and name='{entry.name}' and trashed=false", spaces='drive').execute()
+
+            # update file
+            if response['files'] and localLastModified > driveLastModified:
+                fileId = response['files'][0].get("id")
+                media = MediaFileUpload(entry.path, resumable=True)
+                service.files().update(fileId=fileId, media_body=media).execute()
+                print(yellow + f"Updated: {entry.name}" + reset, end = "\r")
+
+            # upload as new file
+            if not response['files']:
                 fileMetadata = {"name": entry.name, "parents": [curId]}
                 media = MediaFileUpload(entry.path, resumable=True)
                 file = service.files().create(body=fileMetadata, media_body=media, fields="id").execute()
                 print(yellow + f"Uploaded: {entry.name}" + reset, end = "\r")
+
 
 
 def promptConfigs(configs):
@@ -145,8 +177,8 @@ def promptConfigs(configs):
         gameDir = input()
     # print out which files are set to backup/not
     # change the print formatting its ugly
-    backupStr = ("Saves " if configs['backupSaves'] else "") + ("Tray " if configs['backupTray'] else "") + ("accountDataDB.package " if configs['backupAccountData'] else "") + ("Mods " if configs['backupMods'] else "")
-    noBackupStr = ("Saves " if not configs['backupSaves'] else "") + ("Tray " if not configs['backupTray'] else "") + ("accountDataDB.package " if not configs['backupAccountData'] else "") + ("Mods " if not configs['backupMods'] else "")
+    backupStr = ("Saves " if configs['backupSaves'] else "") + ("Tray " if configs['backupTray'] else "") + (f"{ACCT_DATA_FILE} " if configs['backupAccountData'] else "") + ("Mods " if configs['backupMods'] else "")
+    noBackupStr = ("Saves " if not configs['backupSaves'] else "") + ("Tray " if not configs['backupTray'] else "") + (f"{ACCT_DATA_FILE} " if not configs['backupAccountData'] else "") + ("Mods " if not configs['backupMods'] else "")
     if backupStr:
         print("These items will be backed up: " + backupStr)
     else:
@@ -161,9 +193,9 @@ def promptConfigs(configs):
         configs['backupSaves'] = input().lower().strip() == 'y'
         print("Backup tray folder? (y/n)")
         configs['backupTray'] = input().lower().strip() == 'y'
-        print("Backup accountDataDB.package? (y/n)")
+        print(f"Backup {ACCT_DATA_FILE}? (y/n)")
         configs['backupAccountData'] = input().lower().strip() == 'y'
-        print("Backup 'Mods' folder? (y/n)")
+        print("Backup Mods folder? (y/n)")
         configs['backupMods'] = input().lower().strip() == 'y'
     return configs
 
