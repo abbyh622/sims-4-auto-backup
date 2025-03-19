6ef2b75cf0b906a05d6c77a3fa87c289d0669379
abbyh622 <abbyh622@gmail.com>
2025-03-19 00:17:32 -0500

    added refresh token error handling

--- sims4autobackup.py
+++ sims4autobackup.py
@@ -1,6 +1,7 @@
 import json
 import os.path
 import logging
+import tqdm
 from datetime import datetime, timezone
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
@@ -15,6 +16,7 @@ from googleapiclient.errors import HttpError
 
 SCOPES = ["https://www.googleapis.com/auth/drive"]
 ACCT_DATA_FILE = "accountDataDB.package"
+DEFAULT_CONFIGS = {"gameDir": "C:\\Users\\abbyh\\Documents\\Electronic Arts\\The Sims 4", "items": {"backupSaves": True, "backupTray": False, "backupAccountData": True, "backupMods": False}}
 creds = None
 gameDir = os.path.expanduser("~\\Documents\\Electronic Arts\\The Sims 4")   # default game directory, should be universal i think
 
@@ -22,8 +24,8 @@ gameDir = os.path.expanduser("~\\Documents\\Electronic Arts\\The Sims 4")   # de
 red = "\033[31m"
 green = "\033[32m"
 yellow = "\033[33m"
-blue = "\033[34m"
 magenta = "\033[35m"
+ltblue = "\033[36m"
 reset = "\033[0m"       # resets to default
 clearLine = "\033[K"    # clears from cursor to end of line, prevent overlapping if last line longer than current
 
@@ -31,8 +33,9 @@ def main():
     global creds
     global gameDir
 
-    creds = authenticateGoogleDrive()
+
     logging.basicConfig(level=logging.INFO, format="%(message)s")
+    creds = authenticateGoogleDrive()
     
     # get gameDir path and backup settings from config file
     if os.path.exists("config.json"):
@@ -43,10 +46,10 @@ def main():
             json.dump(configs, file) 
     # if no config file, apply defaults and prompt user
     else:
-        defaultConfigs = {"gameDir": gameDir, "backupSaves": True, "backupTray": True, "backupAccountData": True, "backupMods": False}
-        configs = promptConfigs(defaultConfigs)
+        configs = promptConfigs(DEFAULT_CONFIGS)
         with open("config.json", "w") as file:
             json.dump(configs, file)
+    backupItems = configs['items']
 
     try:
         service = build("drive", "v3", credentials=creds)
@@ -61,22 +64,22 @@ def main():
             metadata = {"name": "Sims 4 Backup", "mimeType": "application/vnd.google-apps.folder"}
             file = service.files().create(body=metadata, fields="id").execute()
             folderId = file.get("id")
-
             logging.info("Backup folder not found; created new folder")
         
-        if configs['backupSaves']:
-            logging.info(blue + clearLine + "Uploading save files . . ." + reset)
+
+        if backupItems['backupSaves']:
+            logging.info(ltblue + clearLine + "Uploading save files . . ." + reset)
             backupFolder(service, "saves", folderId)
-        if configs['backupTray']:
-            logging.info(blue + clearLine + "Uploading tray files . . ." + reset)
+        if backupItems['backupTray']:
+            logging.info(ltblue + clearLine + "Uploading tray files . . ." + reset)
             backupFolder(service, "Tray", folderId)
-        if configs['backupMods']:
-            logging.info(blue + clearLine + "Uploading mods folder . . ." + reset)
+        if backupItems['backupMods']:
+            logging.info(ltblue + clearLine + "Uploading mods folder . . ." + reset)
             backupFolder(service, "Mods", folderId)           
-        if configs['backupAccountData']:
+        if backupItems['backupAccountData']:
         # backup accountdatadb
         # not checking modified time bc its a small enough file and seems to update every time game is closed
-            logging.info(blue + clearLine + "Uploading account data file . . ." + reset)
+            logging.info(ltblue + clearLine + "Uploading account data file . . ." + reset)
             localdb = os.path.join(gameDir, ACCT_DATA_FILE)
             if os.path.exists(localdb) and os.path.isfile(localdb):
                 response = service.files().list(q=f"'{folderId}' in parents and name='{ACCT_DATA_FILE}' and trashed=false", spaces='drive').execute()
@@ -85,15 +88,17 @@ def main():
                     fileId = response['files'][0].get("id")
                     media = MediaFileUpload(localdb, resumable=True)
                     service.files().update(fileId=fileId, media_body=media).execute()
-                    print(yellow + f"Uploaded: {os.path.basename(localdb)}" + reset, end = "\r")
+                    print(yellow + f"Uploaded: {ACCT_DATA_FILE}" + reset, end = "\r")
                 # upload as new file
                 else:
-                    metadata = {"name": os.path.basename(localdb), "parents": [folderId]}
+                    metadata = {"name": ACCT_DATA_FILE, "parents": [folderId]}
                     media = MediaFileUpload(localdb, resumable=True)
                     file = service.files().create(body=metadata, media_body=media, fields="id").execute()
-                    print(yellow + f"Uploaded: {os.path.basename(localdb)}" + reset, end = "\r")
+                    print(yellow + f"Uploaded: {os.path.basename(ACCT_DATA_FILE)}" + reset, end = "\r")
+            else:
+                logging.error(red + f"Error: {os.path.basename(ACCT_DATA_FILE)} not found" + reset)
 
-        print(magenta + clearLine + "Backup complete :)" + reset)
+        print(green + clearLine + "Backup complete :)" + reset)
             
     except HttpError as e:
         logging.error(red + f"Error: {str(e)}" + reset)
@@ -103,22 +108,34 @@ def main():
 def authenticateGoogleDrive():
     global creds
     if os.path.exists("token.json"):
-        creds = Credentials.from_authorized_user_file("token.json")
+        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
     if not creds or not creds.valid:
+        # if creds expired, refresh token
         if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
+            logging.info("Refreshing token . . .")
+            try:
+                creds.refresh(Request())
+            # if refresh fails, delete token file and reauthenticate
+            except Exception as e:
+                logging.error(red + f"Error refreshing token: {str(e)}" + reset)
+                os.remove("token.json")
+                creds = None
+        # if no creds, get new token (promps user to authenticate in browser)
+        if not creds:
+            logging.info("Authenticating. . .")
             flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
             creds = flow.run_local_server(port=0)
+            # save token
         with open("token.json", "w") as token:
             token.write(creds.to_json())
     return creds
 
+
 # process for saves, tray, and mods folders
 def backupFolder(service, curFolder, mainFolder): 
-    # implementing later bc mods folder can be different
+
     if curFolder == "Mods":
-        logging.info(red + "Mods folder backup attempted; not yet implemented" + reset)
+        print(red + "Mods folder backup not yet implemented" + reset)
         return
     
     # check if folder exists, get id and datetime last modified
@@ -130,7 +147,7 @@ def backupFolder(service, curFolder, mainFolder):
         file = service.files().create(body=metadata, fields="id").execute()
         curId = file.get("id")
  
-    # get last modified datetime of saves folder
+    # get last modified datetime of drive folder
     response = service.files().list(q=f"'{curId}' in parents", spaces='drive', fields="files(modifiedTime)", orderBy="modifiedTime desc").execute()
 
     # convert to timwzone aware datetime object for comparison
@@ -140,32 +157,31 @@ def backupFolder(service, curFolder, mainFolder):
     else:
         # set to epoch time if no files in folder
         driveLastModified = datetime.fromtimestamp(0, tz=timezone.utc)
-
-    print(f"Last backed up: {str(driveLastModified)}", end = "\r") 
+    # print(f"Last backed up: {str(driveLastModified)}") 
+    
+    localFolder = os.path.join(gameDir, curFolder)  # get local folder path             
+    # get number of files in folder for progress bar
+    numFiles = len(os.listdir(localFolder))                                     # dont think this will be right for mods folder bc nested folders
 
     # use os.scandir() to iterate through each folder 
-    localFolder = os.path.join(gameDir, curFolder)
-    for entry in os.scandir(localFolder):
+    for entry in tqdm.tqdm(os.scandir(localFolder), total=numFiles, position=0):
         # get last modified time
         if entry.is_file():
             localLastModified = datetime.fromtimestamp(os.path.getmtime(entry.path), tz=timezone.utc)
             # check if file exists in drive
             response = service.files().list(q=f"'{curId}' in parents and name='{entry.name}' and trashed=false", spaces='drive').execute()
-
             # update file
             if response['files'] and localLastModified > driveLastModified:
                 fileId = response['files'][0].get("id")
                 media = MediaFileUpload(entry.path, resumable=True)
                 service.files().update(fileId=fileId, media_body=media).execute()
-                print(yellow + f"Updated: {entry.name}" + reset, end = "\r")
-
+                # print(yellow + f"Updated: {entry.name}" + reset, end = "\r")
             # upload as new file
             if not response['files']:
                 fileMetadata = {"name": entry.name, "parents": [curId]}
                 media = MediaFileUpload(entry.path, resumable=True)
                 file = service.files().create(body=fileMetadata, media_body=media, fields="id").execute()
-                print(yellow + f"Uploaded: {entry.name}" + reset, end = "\r")
-
+                # print(yellow + f"Uploaded: {entry.name}" + reset, end = "\r")
 
 
 def promptConfigs(configs):
@@ -175,28 +191,32 @@ def promptConfigs(configs):
     while not os.path.exists(gameDir):
         print(red + "The Sims 4 game directory not found.\nPlease paste the path to your game (should end in 'Documents\\Electronic Arts\\The Sims 4')" + reset)
         gameDir = input()
-    # print out which files are set to backup/not
-    # change the print formatting its ugly
-    backupStr = ("Saves " if configs['backupSaves'] else "") + ("Tray " if configs['backupTray'] else "") + (f"{ACCT_DATA_FILE} " if configs['backupAccountData'] else "") + ("Mods " if configs['backupMods'] else "")
-    noBackupStr = ("Saves " if not configs['backupSaves'] else "") + ("Tray " if not configs['backupTray'] else "") + (f"{ACCT_DATA_FILE} " if not configs['backupAccountData'] else "") + ("Mods " if not configs['backupMods'] else "")
-    if backupStr:
-        print("These items will be backed up: " + backupStr)
+
+    # get lists of items to/not backup and print
+    backupItems = configs['items']
+    willBackup = [k for k,v in backupItems.items() if v]
+    wontBackup = [k for k,v in backupItems.items() if not v]
+    willBackupStr = ", ".join(willBackup)
+    wontBackupStr = ", ".join(wontBackup)
+    if willBackupStr:
+        print("These items will be backed up: " + green + willBackupStr + reset)
+        if wontBackupStr:
+            print("These items will NOT be backed up: " + red + wontBackupStr + reset)
     else:
         print("No items selected for backup.")
-    if noBackupStr:
-        print("These items will NOT be backed up: " + noBackupStr)
+
     # make this so wont continue if another key is pressed
-    print(blue + "Change which items to backup? (y/n)" + reset)
+    print(ltblue + "Change which items to backup? (y/n)" + reset)
     change = input()
     if change.lower().strip() == 'y':
         print("Backup saves folder? (y/n)")
-        configs['backupSaves'] = input().lower().strip() == 'y'
+        backupItems['backupSaves'] = input().lower().strip() == 'y'
         print("Backup tray folder? (y/n)")
-        configs['backupTray'] = input().lower().strip() == 'y'
+        backupItems['backupTray'] = input().lower().strip() == 'y'
         print(f"Backup {ACCT_DATA_FILE}? (y/n)")
-        configs['backupAccountData'] = input().lower().strip() == 'y'
+        backupItems['backupAccountData'] = input().lower().strip() == 'y'
         print("Backup Mods folder? (y/n)")
-        configs['backupMods'] = input().lower().strip() == 'y'
+        backupItems['backupMods'] = input().lower().strip() == 'y'
     return configs
 
 
